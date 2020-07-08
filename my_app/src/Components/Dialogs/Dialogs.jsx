4e1654cfa4b5bbda8e5ad246a1a0955922ccabf9
GwunGG <dima_osipov_2000@bk.ru>
2020-07-08 22:15:17 +0500

    added new styles

--- my_app/src/Components/Dialogs/Dialogs.jsx
+++ my_app/src/Components/Dialogs/Dialogs.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import './Dialogs.css';
+import s from './Dialogs.module.css';
 
 
 const Dialogs = () => {
     return (
-        <div className="content">
-            Dialogs
+        <div className={s.Dialogs_page}>
+            <div className={s.content}>
+                Dialogs
+            </div>
+            <div className={s.dialogs_wrapper}>
+                <div className={s.dialogs_items}>
+                    <div className={s.dialog_item + ' ' + s.active}>Hleb</div>
+                    <div className={s.dialog_item}>Kosty</div>
+                    <div className={s.dialog_item}>An9</div>
+                    <div className={s.dialog_item}>Kolya</div>
+                </div>
+
+                <div className={s.messages_items}>
+                    <div className={s.message_item}>HI</div>
+                    <div className={s.message_item}>How are you?</div>
+                    <div className={s.message_item}>Okay, ty</div>
+                    <div className={s.message_item}>BB</div>
+                    <div className={s.message_item}>W8 pls, bitch</div>
+                </div>
+            </div>
         </div>
     )
 }
--- my_app/src/Components/Navbar/NavBar.jsx
+++ my_app/src/Components/Navbar/NavBar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './NavBar.css';
+import {NavLink} from "react-router-dom" ;
 
 const NavBar=()=>{
     return(
         <div className="NavBar">
-            <div className="NavBar_item"><a className="elments_of_nav" href="/Profile">Profile</a></div>
-            <div className="NavBar_item"><a className="elments_of_nav" href="/Dialogs">Dialogs</a></div>
-            <div className="NavBar_item"><a className="elments_of_nav" href="/Music">Music</a></div>
-            <div className="NavBar_item"><a className="elments_of_nav" href="/News">News</a></div>
-            <div className="NavBar_item"><a className="elments_of_nav" href="/Settings">Settings</a></div>
+            <div className="NavBar_item"><NavLink className="elments_of_nav" to="/Profile">Profile</NavLink></div>
+            <div className="NavBar_item"><NavLink className="elments_of_nav" to="/Dialogs">Dialogs</NavLink></div>
+            <div className="NavBar_item"><NavLink className="elments_of_nav" to="/Music">Music</NavLink></div>
+            <div className="NavBar_item"><NavLink className="elments_of_nav" to="/News">News</NavLink></div>
+            <div className="NavBar_item"><NavLink className="elments_of_nav" to="/Settings">Settings</NavLink></div>
         </div>
     )
 }
